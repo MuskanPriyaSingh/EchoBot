5113bdb36b56e311b35315af97c19a5eacf71447
Suraj kr shaw <krsurajshaw17082004@gmail.com>
2025-04-30 23:49:12 +0530

    init commit

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import SidePanel from './components/SidePanel'
+import Main from './components/Main'
+
+export default function App() {
+  return (
+    <>
+      <SidePanel/>
+      <Main/>
+    </>
+  )
+}
